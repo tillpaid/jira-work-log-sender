fedd45a5383e00d6b4a28d912dba568e446ac196
Oleksandr Maiboroda <tillpaid@gmail.com>
2024-08-02 20:25:15 +0300

    Updated calculation time for table footer

--- internal/ui/table/footer.go
+++ internal/ui/table/footer.go
@@ -6,54 +6,50 @@ import (
 )
 
 func GetFooter(workLogs []model.WorkLog, delimiter string) []string {
-	totalTime := calculateTotalTime(workLogs)
-	leftTime := calculateLeftTime(workLogs)
+	totalInMinutes := getTotalInMinutes(workLogs)
+
+	totalTime := calculateTotalTime(totalInMinutes)
+	leftTime := calculateLeftTime(totalInMinutes)
+
 	totalRow := fmt.Sprintf("%s | %s", totalTime, leftTime)
 
 	return []string{delimiter, totalRow, delimiter}
 }
 
-func calculateTotalTime(workLogs []model.WorkLog) string {
+func calculateTotalTime(totalInMinutes int) string {
 	var hours, minutes int
 
-	for _, workLog := range workLogs {
-		hours += workLog.OriginalTime.Hours
-		minutes += workLog.OriginalTime.Minutes
-	}
-
-	if minutes >= 60 {
-		hours += minutes / 60
-		minutes = minutes % 60
+	if totalInMinutes > 0 {
+		hours = totalInMinutes / 60
+		minutes = totalInMinutes % 60
 	}
 
 	return fmt.Sprintf("Total time: %dh %dm", hours, minutes)
 }
 
-func calculateLeftTime(workLogs []model.WorkLog) string {
-	var totalMinutes int
+func calculateLeftTime(totalInMinutes int) string {
 	var hours, minutes int
-	var minutesWasNegative bool
+	var minusSign string
 
-	for _, workLog := range workLogs {
-		totalMinutes += workLog.OriginalTime.Hours * 60
-		totalMinutes += workLog.OriginalTime.Minutes
-	}
-
-	minutes = 480 - totalMinutes
+	minutes = 480 - totalInMinutes
 	if minutes < 0 {
-		minutesWasNegative = true
 		minutes = minutes * -1
-	}
-
-	if minutes >= 60 {
-		hours += minutes / 60
-		minutes = minutes % 60
-	}
-
-	minusSign := ""
-	if minutesWasNegative {
 		minusSign = "-"
 	}
 
+	hours += minutes / 60
+	minutes = minutes % 60
+
 	return fmt.Sprintf("Left: %s%dh %dm", minusSign, hours, minutes)
+}
+
+func getTotalInMinutes(workLogs []model.WorkLog) int {
+	var total int
+
+	for _, workLog := range workLogs {
+		total += workLog.OriginalTime.Hours * 60
+		total += workLog.OriginalTime.Minutes
+	}
+
+	return total
 }
